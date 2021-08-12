e0f8e9e0208a9a5cc18d38f7df57831602e57423
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-12 19:01:27 -0400

    update version

--- calculator_py/__init__.py
+++ calculator_py/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Kev-in123"
 __title__ = "calculator"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Kev-in123"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from .calc_ import Calc
 
