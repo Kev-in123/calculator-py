7b78d3fd968f43edcdcbb5ba3d433d7e85e0a69e
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-12 21:59:07 -0400

    Update calc_.py

--- calculator_py/calc_.py
+++ calculator_py/calc_.py
@@ -12,7 +12,8 @@ class Calc:
     def divide(self):
       if self.num2 == 0:
         raise InvalidArgument("Division by 0")
-      return self.num1 / self.num2
+      else:
+        return self.num1 / self.num2
 
     def multiply(self):
       return self.num1 * self.num2
@@ -26,12 +27,14 @@ class Calc:
     def sqrt(self):
       if self.num1 < 0:
         raise InvalidArgument("Domain error")
-      return self.num1 ** 0.5
+      else:
+        return self.num1 ** 0.5
 
     def log(self):
       if self.num1 < 0 or self.num2 < 0:
         raise InvalidArgument("Domain error")
-      return math.log(self.num1, self.num2)
+      else:
+        return math.log(self.num1, self.num2)
 
     def gcd(self):
       return math.gcd(self.num1, self.num2)
@@ -42,7 +45,8 @@ class Calc:
     def factorial(self):
       if self.num1 < 0:
         raise InvalidArgument("Domain error")
-      return math.factorial(self.num1)
+      else:
+        return math.factorial(self.num1)
 
     def sin(self):
       return math.sin(self.num1)
@@ -54,16 +58,19 @@ class Calc:
       return math.tan(self.num1)
     
     def asin(self):
-      if -1 > self.num1 > 1:
+      if self.num1 not in range(-1, 2):
         raise InvalidArgument("Domain error")
-      return math.asin(self.num1)
+      else:
+        return math.asin(self.num1)
     
     def acos(self):
-      if -1 > self.num1 > 1:
+      if self.num1 not in range(-1, 2):
         raise InvalidArgument("Domain error")
-      return math.acos(self.num1)
+      else:
+        return math.acos(self.num1)
     
     def atan(self):
-      if -1 > self.num1 > 1:
+      if self.num1 not in range(-1, 2):
         raise InvalidArgument("Domain error")
-      return math.atan(self.num1)
+      else:
+        return math.atan(self.num1)
